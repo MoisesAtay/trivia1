09e8db12839df55ca5f4a1f91bb1232b79831ed8
sesmajal <113271826+MoisesAtay@users.noreply.github.com>
2022-09-17 02:19:47 +0000

    se colocó comentarios

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
+#importo librerias
 import random
 import time
 
-
+#asigno constantes para los colores
 BLACK = '\033[30m'
 RED = '\033[31m'
 GREEN = '\033[32m'
@@ -14,13 +14,15 @@ WHITE = '\033[37m'
 RESET = '\033[39m'
 
 iniciarTrivia = True
+
 print('CULTURA GENERAL')
 print("Bienvenido a mi trivia sobre cultura general")
 print("Pondremos a prueba tus conocimientos")
 
-# la funcion time.sleep es para que espere 5 segundos para que pase al siguiente código
+# la funcion time.sleep es para que espere 5 segundos antes que pase al siguiente código
 time.sleep(5)
 
+#guardo el nombre que va a ingresar el jugador en una variable
 nombre = input("Ingresa tu nombre: ")
 
 print(
@@ -28,32 +30,40 @@ print(
     "responde las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Enter' para enviar tu respuesta:\n"
 )
 time.sleep(1)
-print(YELLOW, "si presionas x, tendrás un mensaje oculto, puede que sea la respuesta pero tendras un puntaje mínimo")
+print(
+    YELLOW,
+    "si presionas x, tendrás un mensaje oculto, puede que sea la respuesta pero tendras un puntaje mínimo"
+)
+
+#mientras la variable iniciarTrivia es True va a seguir el juego
 while iniciarTrivia == True:
+    #se crea un puntaje aleatorio de 0 a 10 puntos
     puntaje = random.randint(0, 10)
     time.sleep(1)
     print(RED, "Tienes ", puntaje, " puntos, cortesía de Trivia:)", RESET)
     time.sleep(1)
 
+    #el contador antes de empezar el juego
     print("La trivia empieza en: ")
     for numeroCarga in range(5, 0, -1):
         print("...", numeroCarga, "...")
         time.sleep(1)
 
     #pregunta 1**********************************
-    #time.sleep(3)
     print(BLUE, '\n1) ¿Cuál es el lugar mas frio de la tierra? ', CYAN)
     print('\t a) la Antártida')
     print('\t b) el Polo Norte')
     print('\t c) Asia')
     print('\t d) Oceania', GREEN)
+    #aca se ingresa la respuesta
     resp = input('respuesta: ')
     print(YELLOW)
 
+    #si la respuesta es una letra diferente de las alternativas, no va a pasar a la siguiente pregunta
     while resp not in ("a", "b", "c", "d", "x"):
         resp = input(
             'debes responder a, b, c o d. Ingresa nuevamente tu respuesta: ')
-
+#para cada respuesta habra puntajes positivo o negativo, segun sea correcto o incorrecto
     if resp == 'a':
         puntaje += random.randint(10, 20)
         print('muy bien')
@@ -66,6 +76,7 @@ while iniciarTrivia == True:
     elif resp == 'd':
         puntaje -= random.randint(5, 10)
         print("Oceania noo!!")
+#si responde con "x" saldra un mensaje secreto que es la respuesta
     else:
         puntaje += 2
         print("la respuesta es tan fria como la Antartida")
@@ -112,6 +123,7 @@ while iniciarTrivia == True:
         resp = input(
             'debes responder a, b, c o d. Ingresa nuevamente tu respuesta: ')
 
+#según las respuestas va a sumar, restar, multiplicar o dividir
     if resp == 'a':
         if puntaje > 0:
             puntaje /= 2
@@ -125,6 +137,7 @@ while iniciarTrivia == True:
         puntaje -= 5
         print("no, para nada")
     elif resp == 'd':
+        #si el puntaje es mayor a 0 multiplica por 2, si es menos de 0 el puntaje se vuelve positivo
         if puntaje > 0:
             puntaje *= 2
         else:
@@ -172,9 +185,10 @@ while iniciarTrivia == True:
 
     time.sleep(2)
     print("\n\nQuiere continuar jugando?: ")
+    #si ingresa "si" el juego se repitirá
     repetirJuego = input(
-        "Ingresa \"si\" para repetir o cualquier tecla para finalizar: ").lower()
+        "Ingresa \"si\" para repetir o cualquier tecla para finalizar: "
+    ).lower()
     if repetirJuego != "si":
         iniciarTrivia = False
         print(f"\nEspero {nombre} hayas pasado muy bien. Hasta pronto!!")
-      
